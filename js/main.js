9d61e83fc8983acb5cf341a0dd291a33012e0551
Seid Temam <seidthedev@gmail.com>
2025-05-21 18:56:03 +0300

    Desktop version done

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+// accordion
+
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accord) => {
+  accord.addEventListener("click", () => {
+    // Close all accordions
+    accordions.forEach((sub) => {
+      sub.classList.remove("active");
+    });
+    accord.classList.add("active");
+
+    // Toggle the clicked one
+    // accord.classList.toggle("active");
+  });
+});
